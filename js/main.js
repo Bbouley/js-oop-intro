83f988feb1fe2c40ea024a7fdf5da9e4ec0a622f
bbouley <b.bouley@hotmail.co.uk>
2015-08-04 11:04:41 -0600

    added new class and methods for the prototype

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-// add scripts
+var Vehicle = function(wheels, color){
+  this.wheels = wheels;
+  this.color = color;
+};
 
-console.log("sanity check!");
+var car = new Vehicle(4, 'red');
+
+Vehicle.prototype.honk = function(){
+  return 'honk!';
+};
+
+Vehicle.prototype.toString = function(){
+  return 'The vehicle has ' + this.wheels + ' wheels and is ' + this.color + '.';
+};
+
+console.log(car);
+console.log(car.honk());
+console.log(car.toString());
+
+var Song = function(songName, bandName){
+  this.songName = songName;
+  this.bandName = bandName;
+};
+
+var awesomeSong = new Song('Never Too Much', 'Luther Vandross');
+
+Song.prototype.countName = function(){
+  return this.songName.split(' ').length;
+};
+
+Song.prototype.createElement = function(){
+
+};
+
+console.log(awesomeSong);
+console.log(awesomeSong.countName());
